1c589ce7a20fa28cf9dd2fa20b1b87c8ba38a614
FabrizioMisseri <misserifabrizio@gmail.com>
2023-03-08 15:35:07 +0100

    add bonus in FizzBuzz

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -8,18 +8,42 @@ package org.lessons.java;
 //Bonus: invece che 100, chiedere all’utente quanti numeri vuole stampare
 // (il valore inserito dall’utente deve essere maggiore di 0 e minore di 1000)
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 1; i < 101; i++) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci i numeri da FizzBuzzare...");
+        int numero = Integer.parseInt(scan.nextLine());
+
+        int i = 1;
+
+        while (i <= numero){
             if (i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            }else {
-                System.out.println(i);
+           } else if (i % 3 == 0) {
+               System.out.println("Fizz");
+          } else if (i % 5 == 0) {
+             System.out.println("Buzz");
+           }else {
+              System.out.println(i);
             }
+            i++;
         }
+
+
+//        for (int i = 1; i < 101; i++) {
+//            if (i % 3 == 0 && i % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            }else {
+//                System.out.println(i);
+//            }
+//        }
     }
 }
